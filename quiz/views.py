2820162c83b58f8257687681718a8ff48a73103e
Sandra Hindskog <sandra@rebelandbird.com>
2015-08-06 10:18:32 +0200

    Sökvägar

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+def startpage(request):
+	return render(request, "quiz/start.html")
+
+def question(request):
+	return render(request, "quiz/question.html")
+
+def completed(request):
+	return render(request, "quiz/complete.html")
 # Create your views here.
