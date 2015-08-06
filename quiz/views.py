b79b5787e74c87080b1a5f88593eff9a39d3aab3
Sandra Hindskog <sandra@rebelandbird.com>
2015-08-06 10:59:55 +0200

    lagra data

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,28 @@
+# coding: utf-8
+
 from django.shortcuts import render
 
+quizzes = {
+	"klassiker": {
+   		"name": u"Klassiska böcker",
+	   	"description": u"Hur bra kan du dina klassiker?"
+	},
+	"fotboll": {
+	   	"name": u"Största fotbollslagen",
+	   	"description": u"Kan du dina lag?"
+	},
+	"kanda-hackare": {
+	    	"name": u"Världens mest kända hackare",
+	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
+}
+
+
+
 def startpage(request):
-	return render(request, "quiz/start.html")
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/start.html", context)
 
 def question(request):
 	return render(request, "quiz/question.html")
