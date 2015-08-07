b044d500a4a63fea84214631f4fdbe527d15e2d5
Sandra Hindskog <sandra@rebelandbird.com>
2015-08-07 16:23:07 +0200

    uppdatering

--- quiz/models.py
+++ quiz/models.py
@@ -8,7 +8,7 @@ class Quiz(models.Model):
 
 class Question(models.Model):
 	quiz = models.ForeignKey(Quiz, related_name="questions")
-	question = models.TextField()
+	question = models.CharField(max_length=100)
 	answer1 = models.CharField(max_length=100)
 	answer2 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
